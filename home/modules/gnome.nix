8baf874c81af69fba5861b7cb118ce86c3d80a29
Asytuyf <nabaoui21@gmail.com>
2026-02-14 18:12:52 +0100

    Automated sync: 2026-02-14 18:12

--- home/modules/gnome.nix
+++ home/modules/gnome.nix
@@ -1,12 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  # Session management via extension
   home.packages = [
     pkgs.gnomeExtensions.another-window-session-manager
   ];
 
-  # GNOME settings
   dconf.settings = {
     "org/gnome/shell/keybindings" = {
       show-screenshot-ui = [ "<Super>s" ];
@@ -14,14 +12,38 @@
 
     "org/gnome/shell" = {
       enabled-extensions = [
+        "user-theme@gnome-shell-extensions.gcampax.github.com"
+        "quicksettings-close-autofocus@gammelsami"
+        "spotify-ad-block@danigm.net"
+        "places-menu@gnome-shell-extensions.gcampax.github.com"
+        "screentospace@dilzhan.dev"
+        "pomodoro@zhuravkovigor.dev"
+        "panel-workspace-scroll@polymeilex.github.io"
+        "music-scales@zenlinux.com"
+        "focuscontrol@itsfernn"
+        "focus@scaryrawr.github.io"
+        "codecal@shihab.kopinfotech.com"
+        "Picture-desktop-widget@GaszokS.github.com"
+        "proton-checker@carvdev.github.com"
+        "perfect-fit@ryliov.work.com"
+        "overviewbackground@github.com.orbitcorrection"
+        "clipqr@drien.com"
+        "claude-code-usage@haletran.com"
+        "weekly-commits@funinkina.is-a.dev"
+        "splashindicator@ochi12.github.com"
+        "pip-on-top@rafostar.github.com"
+        "clipboard-indicator@tudmotu.com"
+        "dash2dock-lite@icedman.github.com"
+        "gnome-mosaic@jardon.github.com"
+        "gTile@vibou"
+        "paperwm@paperwm.github.com"
+        "blur-my-shell@aunetx"
         "another-window-session-manager@nicke.github.io"
       ];
     };
 
-    # Auto-restore previous session at login
     "org/gnome/shell/extensions/another-window-session-manager" = {
       restore-previous-session-at-startup = true;
-      auto-close-apps-and-windows-before-saving = true;
     };
 
     "org/gnome/desktop/session" = {
