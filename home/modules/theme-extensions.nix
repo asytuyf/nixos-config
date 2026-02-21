2e0dbfd38c1fe7873009b922d4b1ecdc71c5729c
Asytuyf <nabaoui21@gmail.com>
2026-02-21 01:51:25 +0100

    Automated sync: 2026-02-21 01:51

--- home/modules/theme-extensions.nix
+++ home/modules/theme-extensions.nix
@@ -1,54 +1,8 @@
 { pkgs, lib, config, ... }:
 
-let
-  # ShaderPaper Extension Derivation (for Shader wallpapers)
-  shaderpaper = pkgs.stdenv.mkDerivation rec {
-    pname = "gnome-shell-extension-shaderpaper";
-    version = "unstable-2024-02-21";
-
-    src = pkgs.fetchurl {
-      url = "https://gitlab.com/raihan2000/shaderpaper/-/archive/08387d4a0a79856aa7c6d58703419675a90eb1e6/shaderpaper-08387d4a0a79856aa7c6d58703419675a90eb1e6.tar.gz";
-      sha256 = "sha256-8snWCtWZ2kHTEhYXXTEwxtbNxPBQEAImCYH5KXHfbqY=";
-    };
-
-    nativeBuildInputs = [
-      pkgs.glib
-      pkgs.gettext
-    ];
-
-    buildInputs = [
-      pkgs.gnome-shell
-      pkgs.gnome-shell-extensions
-    ];
-
-    uuid = "shaderpaper@fogyverse.in";
-
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out/share/gnome-shell/extensions/${uuid}
-      cp -r * $out/share/gnome-shell/extensions/${uuid}
-      runHook postInstall
-    '';
-
-    postFixup = ''
-      glib-compile-schemas $out/share/gnome-shell/extensions/${uuid}/schemas
-    '';
-
-    meta = with lib; {
-      description = "Shader Wallpaper for GNOME";
-      homepage = "https://gitlab.com/raihan2000/shaderpaper";
-      license = licenses.gpl3Only;
-      platforms = platforms.linux;
-    };
-  };
-
-in
 {
   home.packages = with pkgs; [
-    shaderpaper
-    
-    # Dependencies for Video/Animated Wallpapers (Hanabi, etc.)
-    # Kept here so manual installation of such extensions works
+    # Dependencies for Video/Animated Wallpapers (e.g. for manual installation of extensions)
     clapper
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
@@ -64,7 +18,7 @@ in
   dconf.settings = {
     "org/gnome/shell" = {
       enabled-extensions = [
-        "shaderpaper@fogyverse.in"
+        "user-theme@gnome-shell-extensions.gcampax.github.com" # Enable User Themes
       ];
     };
   };
@@ -73,4 +27,5 @@ in
   home.file.".themes/.keep".text = "";
   home.file.".icons/.keep".text = "";
   home.file.".local/share/fonts/.keep".text = "";
+  home.file."Pictures/Wallpapers/Motion/.keep".text = "";
 }
